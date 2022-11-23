18fb0e7b6092365e0fdccd95c1fc21e83cc8eeba
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-11-23 12:37:46 +0530

    hi

---
+++ javasript/sample file/opratos/conditional statement assignment/funct.js
@@ -0,0 +1,17 @@
+/* function  add(a,b)
+{
+    console.log(a)
+    console.log(b)
+
+}
+add("hai","bye","hoi")
+ */
+
+function emp_details(id,name,salary)
+{
+    console.log(id)
+    console.log(name)
+    console.log(salary+5000)
+}
+emp_details(101,"akash",60000)
+emp_details(102,"vignu",50000)
