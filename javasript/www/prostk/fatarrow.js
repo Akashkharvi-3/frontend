5b56afba1fa699ca6bfd841915a5a2680841a2c6
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-11-25 14:40:47 +0530

    hi

---
+++ javasript/www/prostk/fatarrow.js
@@ -0,0 +1,28 @@
+/* var a=()=> 
+{
+    console.log("gm")
+}
+a() */
+
+/* function add(a,b,c)
+{
+    return a+b+c;
+
+
+}
+var res=add(10,20,30)
+{
+    console.log(res)
+}
+*/
+/* 
+//fatarrowfunction
+var add=(a,b,c)=>{
+    return a+b+c;
+}
+console.log(add(10,20,30)) */
+
+
+
+
+
