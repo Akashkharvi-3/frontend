94a0b2539652430d0489256950332ac065753010
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-11-29 11:46:05 +0530

    hi

---
+++ javasript/www/prostk/events/class/1.js
@@ -0,0 +1,16 @@
+class save{
+    min_bal=500;
+    New_ac() {
+        console.log("hello print")
+    }
+    withdr() {}
+    cash() {}
+}
+let c1 = new save();
+
+console.log(c1)
+
+console.log(c1.min_bal)
+c1.New_ac()
+
+console.log(c1.withdr())
