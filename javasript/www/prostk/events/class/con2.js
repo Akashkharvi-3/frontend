f361bc144d61a7d4dc7011401c6b4b122179e9c5
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-11-29 15:17:36 +0530

    hi

---
+++ javasript/www/prostk/events/class/con2.js
@@ -0,0 +1,14 @@
+class account
+{
+    sal=40000;
+   acc_id;
+    name; 
+    constructor(a,b)
+    {
+        this.acc_id = a;
+        this.name = b;
+}}
+var c1=new account(101,"akash")
+var c2=new account(102,"vignesh")
+console.log(c1)
+console.log(c2)
---
+++ javasript/www/prostk/events/class/constructor.js
@@ -0,0 +1,14 @@
+class savings
+{
+    amount=500
+    constructor(){
+        console.log("this is construr")
+    }
+    withdr()
+    {
+    console.log("this is withdraw")
+    }
+    
+}
+let c1=new savings()
+//c1.withdr()
