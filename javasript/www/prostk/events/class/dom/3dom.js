de67c56293bed53840a5e3e4d5470e5a666b62b9
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-06 22:37:25 +0530

    hi

--- javasript/www/prostk/events/class/dom/3dom.js
+++ javasript/www/prostk/events/class/dom/3dom.js
@@ -1,10 +1,14 @@
 function changee()
 {
     document.getElementById('id1').style.backgroundColor='pink'
+    document.getElementById('id1').style.color="red"
 }
 function uptxt()
 {
-   var a= document.getElementById('id2').style.value="hello"
-   document.getElementById('id2').value=a.toUpperCase()
+   //var a= document.getElementById('id2').style.value="hello"
+   
+   var a= document.getElementById('id2').value="hai"
+   
+document.getElementById('id2').value=a.toUpperCase()
 } 
 
