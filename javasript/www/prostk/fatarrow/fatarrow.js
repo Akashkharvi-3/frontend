10ce022998d0bd4f45eacd7089b0e8b9fbc433f3
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-09 12:15:07 +0530

    hi

--- javasript/www/prostk/fatarrow/fatarrow.js
+++ javasript/www/prostk/fatarrow/fatarrow.js
@@ -22,23 +22,23 @@ var add=(a,b,c)=>{
 }
 console.log(add(10,20,30)) */
 
-/* var add = (a,b)=>a+b
-console.log(add(10,20)) */          //short
+var add = (a,b)=>a+b
+console.log(add(10,20))         //short
 
 /* var message = msg=>"hello "+msg
-console.log(message("hi"))                    //short
+console.log(message("hi" */              //short
+  
  
- */
 /*  var mult = (a,b)=>{
     return a*b
 }
 console.log(mult(1,2))
-console.log(mult(2,3) */) 
+console.log(mult(2,3) */
 
 
-var mult=(a,b)=>a*b
+/* var mult=(a,b)=>a*b
 console.log(mult(10,30))
- 
+  */
 
 
 
