e88b3676cdf47abbfccf1970ebfaebd511cce559
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-11 00:15:54 +0530

    hi

---
+++ javasript/otjs/array/swich/1.js
@@ -0,0 +1,28 @@
+let whatday=new Date().getDay()
+//let day;
+switch(whatday)
+{
+    case 0:
+        console.log("sunday")
+        break;
+        case 1:
+        console.log("monday")
+        break;
+        case 2:
+        console.log("tuesday")
+        break;
+        case 3:
+        console.log("wednesday")
+        break;
+        case 4:
+        console.log("thursday")
+        break;
+        case 5:
+            console.log("friday")
+            break;
+            case 6:
+                console.log("saturday")
+                break;
+                    }
+                //      console.log(day)
+                      
