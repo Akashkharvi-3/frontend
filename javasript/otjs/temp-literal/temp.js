12d7fa5cdd18444f8fe64d69f974ab46a8c436e0
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-10 12:23:46 +0530

    hi

---
+++ javasript/otjs/temp-literal/temp.js
@@ -0,0 +1,15 @@
+const name =" akash"
+const age=26
+const job = "developer"
+let html
+html = `<ul>
+    <li>Name:${name}</li>
+    <li>Age:${age}</li>
+    <li>job:${job}</li>
+</ul>`
+document.body.innerHTML=html;
+/* let array = [10,20,30,40,50]
+array.splice(1  ,3 )
+console.log(array)
+
+ */
