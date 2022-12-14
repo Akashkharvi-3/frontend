750751e55b8b90bb11eceed34d33f6625283bcc4
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-14 19:53:33 +0530

    hi

---
+++ javasript/otjs/sample es6/1.js
@@ -0,0 +1,8 @@
+let names =['akash','vignesh','ramakrishna','manjula']
+let n = names.map((name)=>{
+    return name+'kharvi'
+   
+
+})
+
+console.log(n)
---
+++ javasript/www/prostk/14-12-22/2.js
@@ -0,0 +1,18 @@
+class akash{
+    constructor()
+    {
+        console.log("om shree hanumate namaha")
+    }
+}
+class kharvi extends akash
+{
+   
+    constructor()
+
+    {
+       super()
+       console.log("om shree anjaneyaya namaha")
+    }
+}
+let c1=new kharvi()
+
