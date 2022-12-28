7853bbb98bdb09fa38d464943fe9d4a27a7d4607
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2022-12-28 12:30:41 +0530

    hi

---
+++ javasript/JS intriview qutsn/map_using_uppercase.js
@@ -0,0 +1,8 @@
+let employees=["akash","kharvi"]
+/* let b=employees.map((emp)=>{
+return  emp.toUpperCase()
+})
+console.log(b)
+     */
+let b=employees.join(" ")
+console.log(b)
---
+++ javasript/JS intriview qutsn/multiline_string.js
@@ -0,0 +1,7 @@
+/* let str="akash"
+str=str+" kharvi"
+console.log(str) */
+
+let str=`akash
+kharvi`
+console.log(str)
