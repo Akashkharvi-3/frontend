3d93b70536ebc02443cee5c81ea49c5475d3a16c
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-02 19:22:19 +0530

    hi

--- REACT-JS/reacttutorials/react-1/src/App.js
+++ REACT-JS/reacttutorials/react-1/src/App.js
@@ -1,47 +1,18 @@
-import  {useState} from 'react'
+import React,{useState} from "react";
+let msg="vasu"
 let App=()=>{
-  const [todoList,seTtodoList]=useState([])//button
-  
-  const [newTask,setNewTask]=useState("")//input
-  const handleChange=(event)=>{
-    setNewTask(event.target.value)
-  }
-  const addTask=()=>{
-   
-    seTtodoList([...todoList,newTask])
-  }
-  function deleteTask(id) {
-    const removeItem = todoList.filter((task) => {
-      return task.id !== id;
-    });
-    seTtodoList(removeItem);
-  }
-
-  return (
-<div className='container mt-5'>
-    <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange}/>    <button onClick={addTask}>ADD TASK BUTTON</button>
-      </div>
-      <div className='list'>
-        {
-          
-          todoList.map((task)=>{
-            return <div>
-              <ul key={task.id}></ul>
-             
-              <h1>{task}</h1>
-              <button onClick={()=>deleteTask(addTask)}>X</button>
-              </div>
-          })
-        }
-      </div>
-   
-      </div>
-    </div>
-  
-  )
-
+let[abc,seabc]=useState("jai shree ram")
+let akash=()=>{
+  seabc("bbabaguhakna");
 
 }
-export  default App
+return(
+  <div>
+     <h1>messag display:{msg}</h1>
+    <h1>messag:{abc}</h1>
+    <button onClick={akash}>Click</button>
+
+  </div>
+)
+}
+export default App
