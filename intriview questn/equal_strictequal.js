f9d538c83205a2ace32b23d7fdfab37aa6f7b06a
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-08 16:43:20 +0530

    cls

---
+++ intriview questn/equal_strictequal.js
@@ -0,0 +1,19 @@
+let num=20
+if(num==='20')
+{
+    console.log("both are same")
+}
+else{
+    console.log("both are not same")
+}
+
+
+/* what is diffrence between is null and undefined
+both of them empty values
+*/
+
+let a=null
+let b;
+console.log(typeof a)
+console.log(typeof b)
+
---
+++ intriview questn/let_var_const.js
@@ -0,0 +1,28 @@
+
+/* if(true){
+    let a=10;
+    console.log(a)
+}
+
+
+ */
+
+/* function app(){
+     if(true){
+    var a=10;
+    console.log(a)
+}
+console.log(a)
+}
+app()
+console.log(a) */
+
+let a=10
+ a=20
+console.log(a)
+
+
+const kharvi={name:"akash",skills:"frontend developer"}
+kharvi.skills="react";
+kharvi.place="kundapur"
+console.log(kharvi)
