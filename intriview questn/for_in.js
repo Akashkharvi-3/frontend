378754a6210c60891988055d92ec3768e9990261
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-10 15:51:22 +0530

    hi

---
+++ intriview questn/for_in.js
@@ -0,0 +1,28 @@
+ let obj={a:1,
+b:2,c:3}
+for(let o in obj)
+{
+    console.log(obj[o])
+}
+
+/* output
+1
+2
+3
+*/
+ 
+
+//array  for in
+
+ let aku=["a","b","c","d"]
+
+for(let name in aku) // print index value 0 1 2 3 in array   for in concept
+{
+    console.log(aku[name]) 
+}
+
+o/p 
+  a 
+  b
+  c
+  d
