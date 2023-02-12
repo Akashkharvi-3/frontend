6d60cc039a1f4a6de098b391f399503654064130
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-12 18:43:11 +0530

    hi

--- intriview questn/asynchronous.js
+++ intriview questn/asynchronous.js
@@ -13,7 +13,7 @@ function third(){
     console.log(3)
 }
 first()
-second(third) */
+second(third) *//* 
  
 function getuser(a){
     return new Promise((resolve,reject)=>{
@@ -34,4 +34,36 @@ getuser(true).then((response)=>{
     console.log(response)
 }).catch((failure)=>{
     console.log(failure)
-})
+}) */
+/* 
+function getuser(success){
+    return new Promise((resolve,reject)=>{
+        if(success)
+        {
+            resolve("sucess")
+        }
+        else{
+            reject("failure")
+        }
+    },1000)
+}
+getuser(false).then((res)=>{
+    console.log(res)
+
+}).catch((fail)=>{
+console.log(fail)
+}) */
+
+const promise3=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("success"),1000})
+    })
+          
+        promise3.then((response)=>{
+        console.log(response)
+        
+        
+      
+    })
+   
+
