fbb0c49830eb32443841227f7711003d8019ee7f
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-19 20:06:26 +0530

    hi

--- REACT-JS/reacttutorials/react-1/src/Comp/Comp.js
+++ REACT-JS/reacttutorials/react-1/src/Comp/Comp.js
@@ -1,24 +1,24 @@
-import React from "react"
-class Comp extends React.Component{
+import React, { Component } from 'react'
+class Comp extends Component {
     state={
-        msg:"hi"
+        msg:30000,
+        v:0
+            
     }
-    gmHand=(m)=>{
-this.setState({msg:m})
+    Hand=(a)=>{
+        this.setState({msg:30000+a})
+        console.log("incremnet",a)
     }
-
-    render(){
-        return(
-            <div>
-                <h1>wish message:{this.state.msg}</h1>
-                <button onClick={this.gmHand.bind(this,"Good morning")}>gm</button>
-                <button onClick={this.gmHand.bind(this,"Good night")}>gn</button>
-                <button onClick={()=>{
-                    this.setState({msg:"good night sweet dreams"})
-                }}>sd</button>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>initial salary:{this.state.msg}</h1>
+        <button onClick={this.Hand.bind(this,50000)}>hike 50k</button>
+        <button onClick={this.Hand.bind(this,1000000)}>hike 10k</button>
+   <button onClick={()=>{this.setState({msg:0},console.log("increment",this.state.v))}}>hike 0</button>
+      </div>
+    )       
+  }
 }
+
 export default Comp
