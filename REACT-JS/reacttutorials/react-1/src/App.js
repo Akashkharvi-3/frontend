e7b61aa8eb226f3f2a9c24b90e1234f6cfc7dc74
Akashkharvi-3 <akash.kharvi.219@gmail.com>
2023-02-26 23:17:22 +0530

    hi

--- REACT-JS/reacttutorials/react-1/src/App.js
+++ REACT-JS/reacttutorials/react-1/src/App.js
@@ -1,23 +1,49 @@
-import React from "react";
+/* import React, { useState } from 'react';
+import Circle from './Circle/Circle';
 
+const CircleContainer = () => {
+  const [circles, setCircles] = useState([]);
 
-import Login from "./Login/Login";
+  const addCircle = () => {
+    const newCircle = {
+      size: '150px',
+      color: 'green',
+      key: circles.length,
+    };
 
+    setCircles([...circles, newCircle]);
+  };
 
+  return (
+    <div>
+      <button onClick={addCircle}>Add Circle</button>
+      <div>
+        {circles.map(circle => (
+          <Circle {...circle} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
+export default CircleContainer;
+ */
+import React, { useState } from 'react';
 
+function App() {
+  const [circles, setCircles] = useState([]);
 
-const App=()=>{
-  return(
-    <>
-    <Login/>
+  const addCircle = () => {
+    const newCircle = <div style={{  width: '150px', height: '150px', borderRadius: '50%', backgroundColor: 'red', margin: '10px' }}></div>;
+    setCircles([...circles, newCircle]);
+  }
 
-   
-    
-     
-        
-      
-    </>
-  )
+  return (
+    <div style={{ display: "flex"  }}>
+      <button onClick={addCircle}>Add Circle</button>
+      {circles.map((circle, index) => <div key={index}>{circle}</div>)}
+    </div>
+  );
 }
-export default App
+
+export default App;
---
+++ REACT-JS/reacttutorials/react-1/src/Circle/Circle.js
@@ -0,0 +1,15 @@
+/* import React from 'react';
+
+const Circle = ({ size, color, key }) => {
+  const style = {
+    width: size,
+    height: size,
+    borderRadius: '550%',
+    backgroundColor: color,
+    margin: '5px',
+  };
+
+  return <div style={style} key={key}></div>;
+};
+
+export default Circle; */
